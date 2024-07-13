2656e76f032be7bde36629ded64f68e0fba9f9eb
xhide341 <shawnehgn10@gmail.com>
2024-07-13 22:19:29 +0800

    create functionality for buttons

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// modal script
+// dialog script
 const addBookBtn = document.getElementById('add-book-btn');
 const bookDialog = document.getElementById('book-dialog');
 const modalBackground = document.querySelector('.modal-background');
 const cancelBtn = document.getElementById('cancel-btn');
-
 
 addBookBtn.addEventListener('click', () => {
     bookDialog.showModal();
@@ -12,11 +11,6 @@ addBookBtn.addEventListener('click', () => {
 cancelBtn.addEventListener('click', () => {
     bookDialog.close();
 });
-
-
-addBookBtn.addEventListener('click', openForm);
-cancelBtn.addEventListener('click', closeForm);
-modalBackground.addEventListener('click', closeForm);
 
 // form script
 const bookForm = document.getElementById('add-book-form');
@@ -28,26 +22,63 @@ const myLibrary = [];
 
 bookForm.addEventListener('submit', addBookToLibrary);
 
-addBookToLibrary = function(event) {
+function Book(title, author, pageCount, year) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.year = year;
+    this.read = false;
+}
+
+function addBookToLibrary(event) {
     event.preventDefault();
-    const book = {
-        title: bookTitle.value,
-        author: bookAuthor.value,
-        pageCount: pageCount.value,
-        year: bookYear.value,
-    };
+    const book = new Book(bookTitle.value, bookAuthor.value, pageCount.value, bookYear.value);
     myLibrary.push(book);
     bookTitle.value = '';
     bookAuthor.value = '';
     pageCount.value = '';
     bookYear.value = '';
     bookDialog.close();
+    renderBooks();
 }
 
-function Book() {
-  // the constructor...
+function createCard(book, index) {
+    const card = document.createElement('div');
+    card.classList.add('book-item');
+    card.innerHTML = `
+        <h2>${book.title}</h2>
+        <p>${book.author}</p>
+        <p>${book.pageCount} pages</p>
+        <p>${book.year}</p>
+        <div class="btn-books">
+            <button class="btn-status">${book.read ? 'Mark as unread' : 'Mark as read'}</button>
+            <button class="btn-delete">Delete</button>
+        </div>           
+    `;
+
+    const statusBtn = card.querySelector('.btn-status');
+    statusBtn.addEventListener('click', () => {
+        book.read = !book.read;
+        renderBooks();
+    });
+
+    const deleteBtn = card.querySelector('.btn-delete');
+    deleteBtn.addEventListener('click', () => {
+        myLibrary.splice(index, 1);
+        renderBooks();
+    });
+
+    return card;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function renderBooks() {
+    const booksContainer = document.querySelector('.book-container');
+    
+    const existingBooks = booksContainer.querySelectorAll('.book-item:not(.static)');
+    existingBooks.forEach(book => book.remove());
+
+    myLibrary.forEach((book, index) => {
+        const newBookElement = createCard(book, index);
+        booksContainer.insertBefore(newBookElement, booksContainer.lastElementChild);
+    });
 }
