e20016ea546084c906f7d311d43cc52c91316759
xhide341 <shawnehgn10@gmail.com>
2024-07-13 21:06:34 +0800

    create book object constructor

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// modal script
 const addBookBtn = document.getElementById('add-book-btn');
 const bookDialog = document.getElementById('book-dialog');
 const modalBackground = document.querySelector('.modal-background');
@@ -16,3 +17,37 @@ cancelBtn.addEventListener('click', () => {
 addBookBtn.addEventListener('click', openForm);
 cancelBtn.addEventListener('click', closeForm);
 modalBackground.addEventListener('click', closeForm);
+
+// form script
+const bookForm = document.getElementById('add-book-form');
+const bookTitle = document.getElementById('book-title');
+const bookAuthor = document.getElementById('book-author');
+const pageCount = document.getElementById('page-count');
+const bookYear = document.getElementById('book-year');
+const myLibrary = [];
+
+bookForm.addEventListener('submit', addBookToLibrary);
+
+addBookToLibrary = function(event) {
+    event.preventDefault();
+    const book = {
+        title: bookTitle.value,
+        author: bookAuthor.value,
+        pageCount: pageCount.value,
+        year: bookYear.value,
+    };
+    myLibrary.push(book);
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    pageCount.value = '';
+    bookYear.value = '';
+    bookDialog.close();
+}
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
